cde2af3dd84f4afba588652f5cb8a50af7f24211
FabianaRSFF <fabianarsantos@gmail.com>
2022-02-22 19:21:09 -0300

    dados aleatorios para exercicios

---
+++ dados.py
@@ -0,0 +1,26 @@
+produtos = [
+    {'nome': 'p1', 'preço': 13},
+    {'nome': 'p2', 'preço': 55},
+    {'nome': 'p3', 'preço': 33.9},
+    {'nome': 'p4', 'preço': 5.80},
+    {'nome': 'p5', 'preço': 12.90},
+    {'nome': 'p6', 'preço': 11},
+    {'nome': 'p7', 'preço': 44.7},
+    {'nome': 'p8', 'preço': 10},
+    {'nome': 'p9', 'preço': 22.7},
+    {'nome': 'p10', 'preço': 11}
+]
+pessoas = [
+    {'nome': 'Luis', 'idade': 32},
+    {'nome': 'Eduarda', 'idade': 12},
+    {'nome': 'Flavia', 'idade': 15},
+    {'nome': 'Jaira', 'idade': 2},
+    {'nome': 'Luisa', 'idade': 82},
+    {'nome': 'Luana', 'idade': 39},
+    {'nome': 'Rana', 'idade': 52},
+    {'nome': 'Lazaro', 'idade': 40},
+    {'nome': 'Max', 'idade': 22},
+    {'nome': 'Val', 'idade': 32},
+
+]
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
