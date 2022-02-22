c2f6561cc82e9e862219fc553bcb1cf5f0deb613
FabianaRSFF <fabianarsantos@gmail.com>
2022-02-22 14:58:34 -0300

    Lista

---
+++ 40_2split.py
@@ -0,0 +1,12 @@
+# lista = [
+#     [1,2],
+#     [3,4],
+#     [5,6],
+# ]
+# for valor in lista:
+#     # print(valor)
+#     print(valor[0], valor[1])
+
+lista = ['Maria', 'João', 'sil']
+for valor, nome in enumerate(lista):
+    print(valor, nome)
