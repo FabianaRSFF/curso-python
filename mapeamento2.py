f54b1a5d2e82e427b68cea2288d36b24cd311081
FabianaRSFF <fabianarsantos@gmail.com>
2022-02-22 19:25:11 -0300

    importando de dados.py

---
+++ mapeamento2.py
@@ -0,0 +1,11 @@
+from dados import produtos, pessoas, lista
+
+# Utilizando Filter:
+nova_lista = filter(lambda x: x > 5, lista)
+
+#Utilizando list comprehension:
+
+
+nova_lista = [x for x in lista if x > 5]
+print(nova_lista)
+print(list(nova_lista))
