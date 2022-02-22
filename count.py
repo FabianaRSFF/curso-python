11c8dde2e8aa6f29c33b6111b23315a2d440be8c
FabianaRSFF <fabianarsantos@gmail.com>
2022-02-22 18:58:17 -0300

    count

---
+++ count.py
@@ -0,0 +1,10 @@
+"""
+Count itertools
+"""
+from itertools import count
+contador = count(start=0, step=-1)
+for valor in contador:
+    print(round(valor, 2))
+    if valor >= 10 or valor <= -10:
+        break
+    
