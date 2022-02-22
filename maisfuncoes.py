cbecf8f5068aff7a52fb200fa0fa0ad7bf30e9ef
FabianaRSFF <fabianarsantos@gmail.com>
2022-02-22 17:57:45 -0300

    Funcoes

---
+++ maisfuncoes.py
@@ -0,0 +1,21 @@
+variavel = 'casa'
+
+def func():
+    print(variavel)
+
+
+def func2(arg=None):
+    arg = 'sol', 'lua'
+    return arg
+
+
+def func3(arg=None):
+    arg = 'amarelo', 'azul'
+    return arg
+
+
+outra_variavel = func2(arg='sol''lua'), func3(arg='amarelo''azul')
+print(outra_variavel)
+
+
+
