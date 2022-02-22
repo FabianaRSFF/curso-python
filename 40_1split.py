a85af9513935e1736fc5ca630cc45ffd812f3e7b
FabianaRSFF <fabianarsantos@gmail.com>
2022-02-22 14:56:44 -0300

    Split

---
+++ 40_1split.py
@@ -0,0 +1,8 @@
+string = 'O Brasil está uma merda, fora bolsonaro.'
+lista = string.split(' ')
+# string_2 = ','.join(lista)
+# print(lista)
+# print(string_2)
+
+for indice, valor in enumerate(lista):
+    print(indice, valor)
