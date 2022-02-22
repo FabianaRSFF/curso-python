b68bfa4df315d37768bcc8fd61be75a8c72d0bbc
FabianaRSFF <fabianarsantos@gmail.com>
2022-02-22 19:22:31 -0300

    Importando dos dados.py

---
+++ mapeamento.py
@@ -0,0 +1,17 @@
+from dados import produtos, pessoas, lista
+
+#nova_lista = map(lambda x: x * 2, lista) SOLUÇÃO EM MAPEAMENTO
+#SOLUÇÃO EM LIST COMPREHENSION:
+"""nova_lista = (x * 2 for x in lista)
+print(lista)
+print(list(nova_lista))"""
+
+
+def aumenta_preco(p):
+    p['preço'] = round(p['preço'] * 1.05, 2)
+    return p
+
+
+novos_produtos = map(aumenta_preco, produtos)
+for produto in novos_produtos:
+    print(produto)
