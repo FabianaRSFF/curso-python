aa0572fb9060917b6f1cfcb81b6a1aac9814382f
FabianaRSFF <fabianarsantos@gmail.com>
2022-03-01 12:00:53 -0300

    Lifo

---
+++ lifo.py
@@ -0,0 +1,11 @@
+livros = list()
+livros.append('L1')
+livros.append('L2')
+livros.append('L3')
+livros.append('L4')
+livros.append('L5')
+livro_removido = livros.pop()
+
+print(livros, livro_removido)
+
+" Aqui é last in, first out."
