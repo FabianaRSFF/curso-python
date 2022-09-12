4e4635dcf1bc0b29a68def6420661eb3dd2b9a22
Fabiana <fabianarsantos@gmail.com>
2022-09-12 09:42:03 -0300

    typing annotations basic

---
+++ type_annotations/tipagem_simples.py
@@ -0,0 +1,11 @@
+uma_string: str = 'Um valor'
+um_inteiro: int = 245555
+um_float: float = 2.43
+um_boolean: bool = True
+um_set: set = {1, 2, 3}
+uma_lista: list = []
+uma_tupla: tuple = 2, 3, 4
+um_dicionario: dict = {}
+
+print(um_boolean)
+print(um_dicionario)
