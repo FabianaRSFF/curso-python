58511a9b9ead67af0568ba4227d2bbaea9f37c14
Fabiana <fabianarsantos@gmail.com>
2022-09-12 13:57:32 -0300

    tipagem de dicionários

---
+++ type_annotations/tipagem_dicionarios.py
@@ -0,0 +1,11 @@
+um_dict: dict[str, int] = {
+    "A": 0,
+    "B": 1,
+    "C": 2,    
+}
+
+um_dict_de_listas: dict[str, list[int]] = {
+    "A": [1, 2],
+    "B": [2, 3],
+    "X": [5, 7],
+}
---
+++ type_annotations/tipagem_simples2.py
@@ -0,0 +1,11 @@
+def soma(x:int, y:int, z:float) -> float:
+    return x + y + z
+
+
+print(soma(2, 4, 2.9))
+
+
+lista_inteiros: list[int] = [1, 2, 3, 4]
+lista_strings: list[str] = ["a", "d", "o"]
+lista_tuplas: list[tuple] = [(1, "a"), (2, "b")]
+lista_listas_int: list[list[int]] = [[1], [3, 4]]
