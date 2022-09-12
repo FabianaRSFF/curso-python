e338262d2456a82d90a83e679bc5306cd77833ca
Fabiana <fabianarsantos@gmail.com>
2022-09-12 15:16:23 -0300

    exemplos de Structural Pattern Matching

--- Pattern Matching/structural_patterb_match.py
+++ Pattern Matching/structural_patterb_match.py
@@ -11,7 +11,7 @@
 
 execute_command('ls')"""
 
-def execute_command(command: str):
+'''def execute_command(command: str):
     match command:
         case "ls":
             print(' $ listing files')
@@ -23,4 +23,24 @@ def execute_command(command: str):
     print('...rest 0f the code')
 
 
-execute_command('ls')
+execute_command('ls')'''
+
+def execute_command(command: str):
+    match command.split():
+        #case ["ls", path]:
+            #print(' $ listing files from', path)
+        case ["ls", *directories, '--force']:
+            for directory in directories:
+                print(' $ listing files Forced', directory)
+        case ["ls", *directories]:
+            for directory in directories:
+                print(' $ listing files', directory)
+        case ['cd', path1, path2, path3]:
+            print("$ changing directory to", path1, path2, path3)
+        case _:
+            print('$ command not implemented')
+    
+
+execute_command('ls /home /users /ovni --force')
+execute_command('ls /home /users /ovni')
+execute_command('cd /home /users /etc')
